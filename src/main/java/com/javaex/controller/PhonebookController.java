e422b041942181ac56f181878c03c3eb9751ec85
aassdd236 <aassdd236@naver.com>
2024-03-27 17:43:29 +0900

    phonebook

--- src/main/java/com/javaex/controller/PhonebookController.java
+++ src/main/java/com/javaex/controller/PhonebookController.java
@@ -1,14 +1,28 @@
 package com.javaex.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.javaex.service.PhonebookService;
+import com.javaex.util.JsonResult;
+import com.javaex.vo.PersonVo;
 
-@Controller
+@RestController
 public class PhonebookController {
 	
 	@Autowired
 	private PhonebookService phonebookService;
 
+	//리스트 띄우기
+	@GetMapping("/api/list")
+	public JsonResult list() {
+		System.out.println("PhonebookController.list()");
+		
+		List<PersonVo> personList=phonebookService.exeList();
+		
+		return JsonResult.success(personList);
+	}
 }
--- src/main/java/com/javaex/dao/PhonebookDao.java
+++ src/main/java/com/javaex/dao/PhonebookDao.java
@@ -1,12 +1,25 @@
 package com.javaex.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import com.javaex.vo.PersonVo;
 
 @Repository
 public class PhonebookDao {
 
 	@Autowired
 	private SqlSession sqlSession;
+	
+	public List<PersonVo> personSelect() {
+		System.out.println("PhonebookDao.personSelect()");
+
+		List<PersonVo> personList=sqlSession.selectList("phonebook.select"); //namespace.id(phonebook.xml에서)
+		System.out.println(personList);
+
+		return personList;
+	}
 }
