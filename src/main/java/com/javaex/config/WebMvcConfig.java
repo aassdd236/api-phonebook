f5f7cf18ce869b566143c3dd18d948e3f180450d
aassdd236 <aassdd236@naver.com>
2024-04-25 13:00:57 +0900

    .

--- src/main/java/com/javaex/config/WebMvcConfig.java
+++ src/main/java/com/javaex/config/WebMvcConfig.java
@@ -11,7 +11,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**") // 주소는 무조건 api로 시작
         .allowedMethods("GET", "POST", "PUT", "DELETE") // 네가지 경우 허락
-        .allowedOrigins("http://localhost:8080") //vscode에서 주소 허락
+        .allowedOrigins("*") //vscode에서 주소 허락
         .allowedHeaders("*") // 모든 요청해더
         .exposedHeaders("Authorization")// 노출시킬 헤더
         .allowCredentials(true); // 쿠키허용
